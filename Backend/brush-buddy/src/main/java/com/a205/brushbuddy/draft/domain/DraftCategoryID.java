8f50ebe7256509bdfe9f71ee9d4b1b1f14709cb9
yhw991228 <=>
2024-01-22 16:41:40 +0900

    feat : add CategoryEntity

---
+++ Backend/brush-buddy/src/main/java/com/a205/brushbuddy/draft/domain/DraftCategoryID.java
@@ -0,0 +1,21 @@
+package com.a205.brushbuddy.draft.domain;
+
+import com.a205.brushbuddy.user.domain.User;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+
+@Embeddable
+public class DraftCategoryID implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "draft_id")
+    private Draft draft;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+}
