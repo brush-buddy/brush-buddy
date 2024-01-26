335576f4c311a00e06f485df5cc0b239dd1cf779
shinae <summa911@naver.com>
2024-01-26 17:45:45 +0900

    feat : 네브바 초기 ã 설정

---
+++ Frontend/brush-buddy/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+import NavigationComponent from "@/components/NavigationComponent.vue";
+</script>
+
+<template>
+  <NavigationComponent></NavigationComponent>
+</template>
+
+<style scoped>
+
+</style>
