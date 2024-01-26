e59b9114b4f7e0099021d647b7922ff833a9a5cc
백승윤 <fangdol888@naver.com>
2024-01-26 16:56:53 +0900

    feature : 게시판 작성 API 작성 완료

--- Backend/brush-buddy/src/main/java/com/a205/brushbuddy/board/controller/BoardController.java
+++ Backend/brush-buddy/src/main/java/com/a205/brushbuddy/board/controller/BoardController.java
@@ -3,21 +3,25 @@ package com.a205.brushbuddy.board.controller;
 
 import com.a205.brushbuddy.board.dto.BoardListRequestDto;
 import com.a205.brushbuddy.board.dto.BoardWriteRequestDto;
+import com.a205.brushbuddy.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/api/board")
+@RequiredArgsConstructor
 public class BoardController {
+    private final BoardService boardService;
 
     @GetMapping("/list")
     public ResponseEntity<?> getBoardList(BoardListRequestDto requestDto){
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("")
-    public ResponseEntity<?> writeBoard(BoardWriteRequestDto requestDto){
-        
+    @PostMapping("/write")
+    public ResponseEntity<?> writeBoard(@RequestBody BoardWriteRequestDto requestDto){
+        boardService.writeBoard(requestDto);
         return ResponseEntity.ok().build();
     }
 
--- Backend/brush-buddy/src/main/java/com/a205/brushbuddy/board/service/BoardServiceImpl.java
+++ Backend/brush-buddy/src/main/java/com/a205/brushbuddy/board/service/BoardServiceImpl.java
@@ -1,10 +1,11 @@
 package com.a205.brushbuddy.board.service;
 
 import com.a205.brushbuddy.board.domain.Board;
+import com.a205.brushbuddy.board.domain.Image;
 import com.a205.brushbuddy.board.dto.BoardWriteRequestDto;
-import com.a205.brushbuddy.board.repository.BoardRepository;
+import com.a205.brushbuddy.board.repository.*;
+import com.a205.brushbuddy.user.domain.User;
 import com.a205.brushbuddy.util.S3Uploader;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.net.URLConnection;
 import java.util.Base64;
 import java.util.List;
 import java.util.Map;
@@ -21,6 +23,10 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class BoardServiceImpl implements BoardService{
     private final BoardRepository boardRepository;
+    private final HashtagRepository hashtagRepository;
+    private final HeartRepository heartRepository;
+    private final ImageRepository imageRepository;
+    private final ReplyRepository replyRepository;
     private final S3Uploader s3Uploader;
 
     @Override
@@ -29,13 +35,32 @@ public class BoardServiceImpl implements BoardService{
     }
 
     @Override
-    @Transactional
     public boolean writeBoard(BoardWriteRequestDto requestDto) {
-
         try{
+            //Board entity에 필요 데이터 우선 삽입
+            Board entity = Board.builder()
+                    .user(User.builder().userId(1).build()) // TEST CODE
+                    //TODO : JWT 토크에서 사용자 로그 가져오기
+                    .boardTitle(requestDto.getTitle())
+                    .boardContent(requestDto.getContents())
+                    .boardThumbnail("")
+                    .boardWatch(0)
+                    .boardLikeNumber(0)
+                    .build();
 
+            //게시판 저장
+            Board result = boardRepository.save(entity);
+
+            //게시판에 연관된 이미지 하나씩 저장
             for(BoardWriteRequestDto.PhotoDTO photoDTO : requestDto.getPhoto()){
-                byte[] image = Base64.getDecoder().decode(photoDTO.getImg());
+                //base64 디코딩 및 multipartfile로 변환
+
+                //meta 정보와 이미지 정보 분리
+                String delims="[,]";
+                String[] parts = photoDTO.getImg().split(delims);
+
+                //이미지 정보 디코딩 및 바이트스트림으로 변경
+                byte[] image = Base64.getDecoder().decode(parts[1]);
 
                 int totalCnt = 1024;
                 try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(totalCnt)) {
@@ -55,29 +80,52 @@ public class BoardServiceImpl implements BoardService{
                     // ByteArrayOutputStream -> ByteArrayInputStream
                     ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
 
-                    // MultipartFile 객체 생성
-                    MultipartFile multipartFile = new MockMultipartFile("파일명", byteArrayInputStream.readAllBytes());
+                    //이미지 타입 추출
+                    String mimeType = null;
+                    String fileExtension = null;
 
-                    s3Uploader.uploadFiles(multipartFile, "/board"); //s3에 멀티파트 파일로 직접 업로드
+                    try {
+                        mimeType = URLConnection.guessContentTypeFromStream(byteArrayInputStream); //mimeType is something like "image/jpeg"
+                        String delimiter="[/]";
+                        String[] tokens = mimeType.split(delimiter);
+                        fileExtension = tokens[1];
+                    } catch (IOException ioException){
+                        ioException.printStackTrace();
+                    }
+
+                    // MultipartFile 객체 생성
+                    MultipartFile multipartFile = new MockMultipartFile("image"+"."+fileExtension, byteArrayInputStream.readAllBytes());
+
+                    //S3에 이미지 저장하고 url 가져오기
+                    String url = s3Uploader.uploadFiles(multipartFile, "board"); //s3에 멀티파트 파일로 직접 업로드
+
+                    //이미지 엔티티 생성
+                    Image img = Image.builder()
+                            .board(result) // 만든 게시판에 연결하기
+                            .imageOrder(photoDTO.getOrder()) // 순서 유지
+                            .imageUrl(url) // 이미지 s3 url
+                            .build();
+
+                    //썸네일인지 확인하고 넣어주기
+                    if(photoDTO.getOrder() == 1){
+                        result.setBoardThumbnail(url);
+                        boardRepository.save(result);
+                    }
+
+                    //이미지 엔티티 저장
+                    imageRepository.save(img);
                 } catch (IOException e) {
+                    e.printStackTrace();
                     return false;
                 }
-
             }
-            Board entity = Board.builder()
-                    .boardTitle(requestDto.getTitle())
-                    .boardContent(requestDto.getContents())
-                    .boardWatch(0)
-                    .
-                    .build();
 
-            boardRepository.save(entity);
         }catch(Exception e) {
+            e.printStackTrace();
             return false;
         }
 
         return true;
-
     }
 
     @Override
