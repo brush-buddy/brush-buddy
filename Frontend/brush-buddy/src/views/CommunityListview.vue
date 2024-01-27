b4ed5d095a49ba80b669d54e112913197767cf0f
용희원 <yhw991228@ajou.ac.kr>
2024-01-27 16:52:09 +0900

    fix : fix-settings

---
+++ Frontend/brush-buddy/src/views/CommunityListview.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { defineComponent } from "vue3";
+import communitycomponent from "@/components/CommunityComponent.vue";
+const communityList = [];
+</script>
+
+<template>
+  <communitycomponent></communitycomponent>
+</template>
+
+<style scoped></style>
