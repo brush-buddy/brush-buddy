239caf3fb74dd776986bbdadfcff7c00310eda98
fangdol888 <fangdol888@gmail.com>
2024-01-27 20:49:17 +0900

    feature : 게시판 관련 기능 엔드포인트 작성

--- Backend/brush-buddy/src/main/java/com/a205/brushbuddy/board/controller/BoardController.java
+++ Backend/brush-buddy/src/main/java/com/a205/brushbuddy/board/controller/BoardController.java
@@ -1,7 +1,9 @@
 package com.a205.brushbuddy.board.controller;
 
 
+import com.a205.brushbuddy.board.dto.BoardDetailResponseDto;
 import com.a205.brushbuddy.board.dto.BoardListRequestDto;
+import com.a205.brushbuddy.board.dto.BoardModifyRequestDto;
 import com.a205.brushbuddy.board.dto.BoardWriteRequestDto;
 import com.a205.brushbuddy.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
@@ -14,16 +16,68 @@ import org.springframework.web.bind.annotation.*;
 public class BoardController {
     private final BoardService boardService;
 
+    // 게시글 검색 및 리스트 조회
     @GetMapping("/list")
     public ResponseEntity<?> getBoardList(BoardListRequestDto requestDto){
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/write")
+
+    // 게시판 작성
+    @PostMapping("")
     public ResponseEntity<?> writeBoard(@RequestBody BoardWriteRequestDto requestDto){
         boardService.writeBoard(requestDto);
         return ResponseEntity.ok().build();
     }
 
+    // 게시판 상세조회
+    @GetMapping("/{boardId}")
+    public  ResponseEntity<?> detailBoard(@PathVariable long boardId){
+        return ResponseEntity.ok().build();
+    }
+
+
+    //게시판 수정
+    @PutMapping("/{boardId}")
+    public  ResponseEntity<?> modifyBoard(@PathVariable long boardId, @RequestBody BoardModifyRequestDto requestDto){
+        return ResponseEntity.ok().build();
+    }
+
+    //게시판 삭제
+    @DeleteMapping("/{boardId}")
+    public  ResponseEntity<?> deleteBoard(@PathVariable long boardId){
+        return ResponseEntity.ok().build();
+    }
+
+
+    //댓글 리스트 조회
+    @GetMapping("/{boardId}/comments")
+    public  ResponseEntity<?> getComments(@PathVariable long boardId){
+        return ResponseEntity.ok().build();
+    }
+
+    //댓글 작성
+    @PostMapping("/{boardId}/comments")
+    public  ResponseEntity<?> writComment(@PathVariable long boardId){
+        return ResponseEntity.ok().build();
+    }
+
+    //댓글 삭제
+    @DeleteMapping("/{boardId}/comments/{commentsId}")
+    public  ResponseEntity<?> deleteComments(@PathVariable long boardId, @PathVariable long commentsId){
+        return ResponseEntity.ok().build();
+    }
+
+    //좋아요 추가
+    @PostMapping("/{boardId}/heart")
+    public  ResponseEntity<?> addHeart(@PathVariable long boardId){
+        return ResponseEntity.ok().build();
+    }
+
+    //좋아요 삭제
+    @DeleteMapping ("/{boardId}/heart")
+    public  ResponseEntity<?> deleteHeart(@PathVariable long boardId){
+        return ResponseEntity.ok().build();
+    }
 
 }
