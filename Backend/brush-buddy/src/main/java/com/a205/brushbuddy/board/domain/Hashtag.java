bc7708a9ec97c38ad39eb810300076a02c6107e3
fangdol888 <fangdol888@gmail.com>
2024-01-27 21:59:34 +0900

    fix : 해시태그 테이블 엔티티 선언 오류로 복합키 관계 수정

--- Backend/brush-buddy/src/main/java/com/a205/brushbuddy/board/domain/Hashtag.java
+++ Backend/brush-buddy/src/main/java/com/a205/brushbuddy/board/domain/Hashtag.java
@@ -1,20 +1,21 @@
 package com.a205.brushbuddy.board.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "hashtag")
 public class Hashtag {
 
 	@EmbeddedId
 	private HashtagPK id;
-
-	@MapsId("boardId")
-	@ManyToOne(fetch = FetchType.LAZY) // Hashtag(Many) : Board(One)
-	@JoinColumn(name = "board_id", nullable = false)
-	private Board board;
 
 	@Column(name = "hashtag_serial_number", nullable = false)
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
