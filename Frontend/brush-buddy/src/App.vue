b07de161253a02f0ea691d5554b6b91ee6656935
용희원 <yhw991228@ajou.ac.kr>
2024-01-27 16:52:09 +0900

    fix : fix-settings

--- Frontend/brush-buddy/src/App.vue
+++ Frontend/brush-buddy/src/App.vue
@@ -1,22 +1,25 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
+import NavigationComponent from "./components/NavigationComponent.vue";
+import CommunityListview from "./views/CommunityListview.vue";
 </script>
 
 <template>
   <header>
-
-    <div class="wrapper">
-
-    </div>
+    <div class="wrapper"></div>
   </header>
 
   <body>
-      <HomeView></HomeView>
+    <CommunityListview></CommunityListview>
+    <NavigationComponent></NavigationComponent>
   </body>
-
 </template>
 
 <style scoped>
-
+NavigationComponent {
+  width: 375px;
+  height: 50px;
+  flex-shrink: 0;
+}
 </style>
---
+++ Frontend/brush-buddy/src/components/CommunityComponent.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div id="communitycomponent"></div>
+</template>
+
+<style scoped>
+#communitiycomponent {
+  width: 300px;
+  height: 400px;
+  flex-shrink: 0;
+}
+</style>
