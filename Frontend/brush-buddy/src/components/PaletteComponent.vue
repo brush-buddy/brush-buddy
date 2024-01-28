cde4993585ef518bba82af92800af99d6819f590
이신애 <summa911@naver.com>
2024-01-29 07:17:06 +0900

    feat : navbar view 추가

---
+++ Frontend/brush-buddy/src/components/PaletteComponent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="fixed_width">
+        <p>palette</p>
+    </div>
+</template>
+
+<style scoped>
+.fixed-width {
+  width: 375px;
+}
+
+</style>
