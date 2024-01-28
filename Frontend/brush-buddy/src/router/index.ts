f8acc7c4f1e45be34b22f41f030965d79b375ae7
이신애 <summa911@naver.com>
2024-01-29 07:17:06 +0900

    feat : navbar view 추가

--- Frontend/brush-buddy/src/router/index.ts
+++ Frontend/brush-buddy/src/router/index.ts
@@ -1,9 +1,34 @@
+import CommunityViewVue from '@/views/CommunityView.vue'
+import DiaryVIewVue from '@/views/DiaryVIew.vue'
+import HomeViewVue from '@/views/HomeView.vue'
+import SearchViewVue from '@/views/SearchView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    
+      {
+        path: '/',
+        name: 'HomeView',
+        component: HomeViewVue
+      },
+      {
+        path: '/',
+        name: 'CommunityView',
+        component: CommunityViewVue
+      },
+      {
+        path: '/',
+        name: 'DiaryView',
+        component: DiaryVIewVue
+      },
+      {
+        path: '/',
+        name: 'SearchView',
+        component: SearchViewVue
+      }
 
   ]
 })
--- Frontend/brush-buddy/src/views/HomeView.vue
+++ Frontend/brush-buddy/src/views/HomeView.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 
 import NavigationComponent from "@/components/NavigationComponent.vue";
+import PaletteComponent from "@/components/PaletteComponent.vue";
 </script>
 
 <template>
   <NavigationComponent></NavigationComponent>
+
+  <PaletteComponent></PaletteComponent>
 </template>
 
 <style scoped>
