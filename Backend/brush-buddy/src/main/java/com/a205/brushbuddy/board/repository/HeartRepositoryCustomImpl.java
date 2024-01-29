1ca6ab8254a0fdb6c849de8662b4f3544db081a3
백승윤 <fangdol888@naver.com>
2024-01-29 13:35:30 +0900

    fix : 게시판 heart 추가 로직 오류 수정

--- Backend/brush-buddy/src/main/java/com/a205/brushbuddy/board/repository/HeartRepositoryCustomImpl.java
+++ Backend/brush-buddy/src/main/java/com/a205/brushbuddy/board/repository/HeartRepositoryCustomImpl.java
@@ -1,21 +1,28 @@
 package com.a205.brushbuddy.board.repository;
 
-import com.a205.brushbuddy.board.domain.QHeart;
-import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.a205.brushbuddy.board.domain.Board;
+import com.a205.brushbuddy.board.domain.Heart;
+import com.a205.brushbuddy.board.domain.HeartId;
+import com.a205.brushbuddy.user.domain.User;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 
 
 @RequiredArgsConstructor
 public class HeartRepositoryCustomImpl implements HeartRepositoryCustom{
 
-    private final JPAQueryFactory query;
+//    private final JPAQueryFactory query;
+    private final EntityManager em;
 
     @Override
     public boolean insertHeart(Integer userId, Long boardId){
-        QHeart heart = QHeart.heart;
-        query.insert(heart)
-                .columns(heart.heartId.user, heart.heartId.board)
-                .values(userId, boardId);
+        em.persist(Heart.builder().heartId(
+                HeartId.builder()
+                        .board(Board.builder().boardId(boardId).build())
+                        .user(User.builder().userId(userId).build())
+                        .build()
+        ).build());
+        em.flush();
         return true;
     }
 }
