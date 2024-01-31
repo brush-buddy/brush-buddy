b4ef70102d9495464efd22befcfdc50a5f2af77e
백승윤 <fangdol888@naver.com>
2024-01-31 14:14:48 +0900

    feature : 마이페이지 서비스 구현 완료

--- Backend/brush-buddy/src/main/java/com/a205/brushbuddy/mypage/service/MypageServiceImpl.java
+++ Backend/brush-buddy/src/main/java/com/a205/brushbuddy/mypage/service/MypageServiceImpl.java
@@ -2,13 +2,12 @@ package com.a205.brushbuddy.mypage.service;
 
 
 import com.a205.brushbuddy.board.domain.Board;
-import com.a205.brushbuddy.board.repository.BoardRepository;
-import com.a205.brushbuddy.board.repository.HeartRepository;
-import com.a205.brushbuddy.draft.repository.Draft.DraftRepository;
+import com.a205.brushbuddy.draft.domain.Draft;
 import com.a205.brushbuddy.mypage.dto.response.MypageBookmarkedDraftListResponseDto;
 import com.a205.brushbuddy.mypage.dto.response.MypageGeneratedDraftListResponseDto;
 import com.a205.brushbuddy.mypage.dto.response.MypageHeartBoardListResponseDto;
 import com.a205.brushbuddy.mypage.dto.response.MypagePurchasedDraftListResponseDto;
+import com.a205.brushbuddy.mypage.repository.MypageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,13 +16,11 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class MypageServiceImpl implements MypageService{
-    private final DraftRepository draftRepository;
-    private final BoardRepository boardRepository;
-    private final HeartRepository heartRepository;
+    private final MypageRepository mypageRepository;
 
     @Override
-    public MypageHeartBoardListResponseDto getHeartBoardList(Integer userId,String search, Pageable pageable) { // 좋아요 누른 게시글 리스트 조회
-       Page<Board> result =  heartRepository.getHeartBoardList(userId, search, pageable);
+    public MypageHeartBoardListResponseDto getHeartBoardList(Integer userId, String search, Pageable pageable) { // 좋아요 누른 게시글 리스트 조회
+       Page<Board> result =  mypageRepository.getHeartBoardList(userId, search, pageable);
         return MypageHeartBoardListResponseDto.builder()
                 .pageNum(result.getNumber()+1)
                 .length(result.getNumberOfElements())
@@ -41,19 +38,54 @@ public class MypageServiceImpl implements MypageService{
     }
 
     @Override
-    public MypageGeneratedDraftListResponseDto getGeneratedBoardList(Integer userId,String search, Pageable pageable){ // 생성한 도안 리스트 조회
-
+    public MypageGeneratedDraftListResponseDto getGeneratedBoardList(Integer userId, String search, Pageable pageable){ // 생성한 도안 리스트 조회
+        Page<Draft> result =  mypageRepository.getGeneratedDraftList(userId, search, pageable);
         return MypageGeneratedDraftListResponseDto.builder()
+                .pageNum(result.getNumber()+1)
+                .length(result.getNumberOfElements())
+                .totalPage(result.getTotalPages())
+                .drafts(result.getContent().stream().map(m-> MypageGeneratedDraftListResponseDto.DraftDto.builder()
+                                .draftId(m.getDraftId())
+                                .draftThumbnail(m.getDraftThumbnail())
+                                .draftTimestamp(m.getDraftTimestamp())
+                        .build())
+                        .toList())
                 .build();
     }
 
     @Override
     public MypageBookmarkedDraftListResponseDto getBookmarkedBoardList(Integer userId,String search, Pageable pageable) {// 북마크한 도안 리스트 조회
-        return MypageBookmarkedDraftListResponseDto.builder().build();
+        Page<Draft> result =  mypageRepository.getBookmarkedDraftList(userId, search, pageable);
+        return MypageBookmarkedDraftListResponseDto.builder()
+                .pageNum(result.getNumber()+1)
+                .length(result.getNumberOfElements())
+                .drafts(result.getContent().stream().map(m-> MypageBookmarkedDraftListResponseDto.DraftDto.builder()
+                                .draftId(m.getDraftId())
+                                .draftThumbnail(m.getDraftThumbnail())
+                                .draftTimestamp(m.getDraftTimestamp())
+                                .draftBookmark(m.getDraftBookmark())
+                                .draftDownload(m.getDraftDownload())
+                                .build())
+                        .toList())
+                .totalPage(result.getTotalPages())
+                .build();
     }
 
     @Override
     public MypagePurchasedDraftListResponseDto getPurchasedDraftList(Integer userId,String search, Pageable pageable){//구매한 도안 내역 리스트 조회
-        return MypagePurchasedDraftListResponseDto.builder().build();
+        Page<Draft> result =  mypageRepository.getPurchasedDraftList(userId, search, pageable);
+        return MypagePurchasedDraftListResponseDto.builder()
+                .pageNum(result.getNumber()+1)
+                .length(result.getNumberOfElements())
+                .totalPage(result.getTotalPages())
+                .drafts(result.getContent().stream().map(m-> MypagePurchasedDraftListResponseDto.DraftDto.builder()
+                                .draftId(m.getDraftId())
+                                .draftThumbnail(m.getDraftThumbnail())
+                                .draftTimestamp(m.getDraftTimestamp())
+                                .draftBookmark(m.getDraftBookmark())
+                                .draftDownload(m.getDraftDownload())
+                                .build())
+                        .toList())
+                .build();
     }
 }
