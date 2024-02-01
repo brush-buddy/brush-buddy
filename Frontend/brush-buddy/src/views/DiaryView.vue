8465cd90e90c33e110f632b63d2699e0559c9d32
전소민 <goflvhxj96@naver.com>
2024-02-01 12:47:38 +0900

    feat: 마이페이지 프로필 화면 작성

--- Frontend/brush-buddy/src/views/DiaryView.vue
+++ Frontend/brush-buddy/src/views/DiaryView.vue
@@ -1,14 +1,84 @@
 <template>
     <div>
-        <p>diary</p>
+        <div>
+            <ComponentProfile />
+        </div>
+    <div class="sticky">메뉴탭</div>
+    <div>div4</div>
+    <div>div5</div>
+    <div>div6</div>
+    <div>div7</div>
+    <div>div8</div>
+    <div>div8</div>
+    <div>div9</div>
+    <div>div10</div>
+    <div>div11</div>
+    <div>div12</div>
+    <div>div13</div>
+    <div>div9</div>
+    <div>div10</div>
+    <div>div11</div>
+    <div>div12</div>
+    <div>div13</div>
+    <div>div14</div>
+    <div>div15</div>
+    <div>div9</div>
+    <div>div10</div>
+    <div>div11</div>
+    <div>div12</div>
+    <div>div13</div>
+    <div>div14</div>
+    <div>div15</div>
+    <div>div14</div>
+    <div>div15</div>
+    <div>div16</div>
+    <div>div8</div>
+    <div>div9</div>
+    <div>div10</div>
+    <div>div11</div>
+    <div>div12</div>
+    <div>div13</div>
+    <div>div14</div>
+    <div>div15</div>
+    <div>div16</div>
+    <div>div8</div>
+    <div>div9</div>
+    <div>div10</div>
+    <div>div11</div>
+    <div>div12</div>
+    <div>div13</div>
+    <div>div14</div>
+    <div>div15</div>
+    <div>div16</div>
+    <div>div8</div>
+    <div>div9</div>
+    <div>div10</div>
+    <div>div11</div>
+    <div>div12</div>
+    <div>div13</div>
+    <div>div14</div>
+    <div>div8</div>
+    <div>div9</div>
+    <div>div10</div>
+    <div>div11</div>
+    <div>div12</div>
+    <div>div13</div>
+    <div>div14</div>
+
     </div>
 </template>
 
 <script setup lang="ts">
-
+import ComponentProfile from "../components/Diary/ComponentProfile.vue"
 </script>
 
 <style scoped>
 
+    .sticky{
+        background-color: yellow;
+        position:sticky;
+        top:0px;
+        line-height: 100px;
+    }
 
 </style>
