49002f21a865d7afafa09598fe85cf5ac24446d0
전소민 <goflvhxj96@naver.com>
2024-02-01 13:23:48 +0900

    fix: 정렬 수정

--- Frontend/brush-buddy/src/App.vue
+++ Frontend/brush-buddy/src/App.vue
@@ -11,7 +11,6 @@ import CommunityListview from "./views/CommunityListview.vue";
     <div class="contentWrapper">
         <RouterView/>
         <NavigationComponent />
-        <CommunityListview></CommunityListview>
     </div>
   </div>
 
