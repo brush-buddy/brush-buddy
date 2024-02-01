c0df46f962ee532b90e0b6b64040fe96542e6d0f
yhw991228 <=>
2024-02-01 12:06:11 +0900

    feature : add componentDetail

---
+++ Frontend/brush-buddy/src/views/CommunityDetailView.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        
+        <p class = "communityTitle">{{community.title}}</p>
+        
+        <div>
+
+        </div>
+        <!-- <img :src="community.photo[0]" alt=""> -->
+        <p>{{community.contents}}</p>
+        
+        <p>{{community}}</p>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+import { onMounted, inject, ref } from 'vue';
+import axios from 'axios';
+const route = useRoute()
+const boardId = route.params.id
+const community = ref<any>(null)
+console.log(route.params)
+onMounted(() => {
+  axios({
+    baseURL: '',
+    method: 'get',
+    url: 'http://localhost:8080/v1/api/board/' + boardId, // URL에 한글이 포함될 경우 인코딩
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8'
+    }
+  }).then(function (response : any) {
+    console.log(response.data)
+    community.value = response.data;
+  })
+})
+</script>
+
+<style scoped>
+
+</style>
--- Frontend/brush-buddy/src/views/CommunityListview.vue
+++ Frontend/brush-buddy/src/views/CommunityListview.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <CommunityComponent :board-thumbnail="boardThumbnailData" />
+    <CommunityComponent :board-thumbnail="boardThumbnailData[1]" />
   </div>
 </template>
 
@@ -9,11 +9,17 @@ import { ref } from 'vue';
 import CommunityComponent from '../components/CommunityComponent.vue';
 import { BoardThumbnail } from '../api/types';
 
-const boardThumbnailData = ref<BoardThumbnail>({
+const boardThumbnailData = ref<BoardThumbnail[]>([{
   boardId: '1',
   boardTitle: 'Example Board',
-  thumbnail: 'image-url.jpg',
+  thumbnail: 'https://picsum.photos/200/300',
   likeNumber: 100,
   views: 500
-});
+},{
+  boardId: '1',
+  boardTitle: 'Example Board',
+  thumbnail: 'https://picsum.photos/200/300',
+  likeNumber: 100,
+  views: 300
+}]);
 </script>
