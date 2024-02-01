3268d901ab94e217fc6390b4ee9c66ddd75388c4
yhw991228 <=>
2024-02-01 18:31:05 +0900

    fix : fix front for build

--- Frontend/brush-buddy/src/main.ts
+++ Frontend/brush-buddy/src/main.ts
@@ -1,10 +1,9 @@
 import App from './App.vue';
 import router from './router';
-import stores from "@/stores/index";
 import { createApp } from 'vue';
 const app = createApp(App);
 
-app.use(router).use(stores);
+app.use(router);
 
 app.mount('#app');
 
