a5629dee147ff65b5b4b57c554692a088e62d54c
전소민 <goflvhxj96@naver.com>
2024-02-02 16:19:08 +0900

    feat: infinite scroll 구현

--- Frontend/brush-buddy/src/views/DiaryView.vue
+++ Frontend/brush-buddy/src/views/DiaryView.vue
@@ -1,90 +1,123 @@
 <template>
+  <div>
     <div>
-        <div>
-            <CProfile />
-        </div>
+      <CProfile />
+    </div>
     <div class="sticky">
-        <div id="tab">
+      <div id="tab">
         <div class="tabButton">만든 도안</div>
         <div class="tabButton">구매 도안</div>
         <div class="tabButton">북마크 도안</div>
-        <div class="tabButton" @click="getHeartList">찜한 글</div>
+        <div class="tabButton" @click="getHeartList(0)">찜한 글</div>
+      </div>
     </div>
-
-    </div>
-        <template v-for="(board, i) in heartList" :key="i">
+    <!-- <template v-for="(board, i) in heartList" :key="i">
             <CCard :board="board"/>
-        </template>
-    </div>
+        </template> -->
+    <v-infinite-scroll :height="300" :items="items" :onLoad="load">
+      <template v-for="(item, index) in items" :key="item">
+        <div :class="['pa-2', index % 2 === 0 ? 'bg-grey-lighten-2' : '']">
+          <!-- Item #{{ item }} -->
+          <CCard :board="item"/>
+        </div>
+      </template>
+    </v-infinite-scroll>
+  </div>
 </template>
 
 <script setup lang="ts">
-import CProfile from "../components/Diary/CProfile.vue"
-import CCard from "../components/Diary/CCard.vue"
+import CProfile from "../components/Diary/CProfile.vue";
+import CCard from "../components/Diary/CCard.vue";
 import axios from "axios";
 import { ref, onMounted } from "vue";
 // import InfiniteLoading from "infinite-loading-vue3-ts";
 
-interface HeartListRes{
-    boards: {
-        boardId: number,
-        boardTitle: string,
-        createdAt: string,
-        likeNumber: number,
-        thumbnail: string,
-        views: number
-    };
-    pageNum: number;
-    length: number;
-    totalPage: number;
+// HeartList 불러오기
+interface HeartListRes {
+  boards: {
+    boardId: number;
+    boardTitle: string;
+    createdAt: string;
+    likeNumber: number;
+    thumbnail: string;
+    views: number;
+  };
+  pageNum: number;
+  length: number;
+  totalPage: number;
 }
-interface HeartList {
-    boardId: number,
-    boardTitle: string,
-    createdAt: string,
-    likeNumber: number,
-    thumbnail: string,
-    views: number
-}
+
 const listNum = ref(3);
-const pageNum = ref(1);
+const pageNum = ref(0);
 const heartList = ref([]);
-const getHeartList = async (): Promise<HeartListRes> => {
-    console.log("getHeartList called");
-    try{
-        // const heartListGet = await axios.get<HeartListRes>(`http://localhost:8080/v1/api/mypage/heart/list?listNum=${listNum.value}&pageNum=${pageNum.value}`)
-        const heartListGet = await axios({
-            method:"get",
-            url: `http://localhost:8080/v1/api/mypage/heart/list?listNum=${listNum.value}&pageNum=${pageNum.value}`,
-            headers: {
-                "Content-Type": "application/json; charset=utf-8",
-                // Authorization: localStorage.getItem("token")
-            },
-        });
-        heartList.value = heartListGet.data.boards;
-        return heartListGet.data.boards;
-    } catch(err){
-        console.log("api 호출 중 오류 발생",err);
-        return Promise.reject(err);
-    }
+const totalPage = ref(0);
+const getHeartList = async (page: number): Promise<HeartListRes> => {
+  console.log("getHeartList called");
+  try {
+    
+    const heartListGet = await axios({
+      method: "get",
+      url: `http://localhost:8080/v1/api/mypage/heart/list?listNum=${listNum.value}&pageNum=${page}`,
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        // Authorization: localStorage.getItem("token")
+      },
+    });
+    heartList.value = heartListGet.data.boards;
+    totalPage.value = heartListGet.data.totalPage;
+    return heartListGet.data.boards;
+  } catch (err) {
+    console.log("api 호출 중 오류 발생", err);
+    return Promise.reject(err);
+  }
 };
 
+// 무한 스크롤 구현 
+const items = ref<number[]>([])
 
+const api = async () => {
+    pageNum.value = pageNum.value + 1;
+    console.log("pageNum ", pageNum.value);
+    console.log("listNum ", listNum.value);
+  return new Promise<number>(resolve => {
+    setTimeout(() => {
+      resolve(pageNum.value);
+    }, 1000)
+  })
+}
+
+const load = async ({ done }: { done: (status: string) => void }) => {
+    try{
+            // Perform API call => pageNum update
+            const res = await api()
+            const resList = await getHeartList(res)
+            items.value.push(...resList)
+            console.log("items: ", items.value);
+
+        if(totalPage.value > pageNum.value){
+            done('ok')
+        }else{
+            done('empty')
+        }
+    } catch(error){
+        console.error("Error during load:", error);
+        done('error')
+    }
+
+}
 
 </script>
 
 <style scoped>
-
-.sticky{
-    position:sticky;
-    top:0px;
-    line-height: 4rem;
-    background-color: #ffffff;
+.sticky {
+  position: sticky;
+  top: 0px;
+  line-height: 4rem;
+  background-color: #ffffff;
 }
-#tab{
-display: flex;
-justify-content: space-around;
-align-items: center;
+#tab {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 }
-
 </style>
