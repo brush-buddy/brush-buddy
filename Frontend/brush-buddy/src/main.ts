e09c2dbd644935e39377349b33d93dc83590b8ea
전소민 <goflvhxj96@naver.com>
2024-02-02 12:54:17 +0900

    chore: main.ts 보충

--- Frontend/brush-buddy/src/main.ts
+++ Frontend/brush-buddy/src/main.ts
@@ -4,20 +4,20 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia'
 const app = createApp(App);
 
-// // Vuetify
-// import { createVuetify } from 'vuetify'
-// import * as components from 'vuetify/components'
-// import * as directives from 'vuetify/directives'
+// Vuetify
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-// const vuetify = createVuetify({
-//   components,
-//   directives
-// })
+const vuetify = createVuetify({
+  components,
+  directives
+})
 
 
 app.use(createPinia());
 app.use(router);
-// app.use(vuetify);
+app.use(vuetify);
 
 app.mount('#app');
 
