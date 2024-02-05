0ea01a13892a0a079defb722ead6136153d26cca
전소민 <goflvhxj96@naver.com>
2024-02-05 09:30:34 +0900

    feat: 무한스크롤 첫 로딩 추가

--- Frontend/brush-buddy/src/views/DiaryView.vue
+++ Frontend/brush-buddy/src/views/DiaryView.vue
@@ -12,19 +12,34 @@
 import CProfile from "../components/Diary/CProfile.vue";
 import CListTab from "../components/Diary/CListTab.vue";
 import { RouterView } from 'vue-router'
+import { ref } from 'vue'
+import axios from 'axios';
+import { provide } from 'vue'
+// const heartList = ref([]);
 
+// const provideHeartList = async () => {
+//   try {
+//     const response = await axios.get('http://localhost:8080/api/v1/mypage/heart/list?listNum=3&pageNum=1', {
+//       headers: {
+//         'Content-Type': 'application/json; charset=utf-8'
+//       }
+//     });
+
+//     console.log("최초호출");
+//     console.log(response.data);
+
+//     heartList.value = response.data.boards;
+//   } catch (error) {
+//     console.error('Error fetching heart list:', error);
+//   }
+// };
+
+// provide('heartList', heartList);
+
+// // Call the provideHeartList method when the component is mounted
+// provideHeartList();
 </script>
 
 <style scoped>
-/* .sticky {
-  position: sticky;
-  top: 0px;
-  line-height: 4rem;
-  background-color: #ffffff;
-}
-#tab {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-} */
+
 </style>
