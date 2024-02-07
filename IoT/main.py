d7bde29205969a9f6e5045746e8a70782ebbea63
백승윤 <fangdol888@gmail.com>
2024-02-07 23:56:46 +0900

    feature  : websocket 서버 기본 코드

---
+++ IoT/main.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI, WebSocket
+import asyncio
+import asyncssh
+from device import devices  
+import json
+
+app = FastAPI()
+
+async def runSshCommand(ipAddress, username, keyFilename, command):
+    async with asyncssh.connect(ipAddress, username=username, client_keys=[keyFilename]) as conn:
+        result = await conn.run(command, check=True)
+        return result.stdout, result.stderr
+
+@app.websocket("/ws/register")
+async def websocketRegister(websocket : WebSocket):
+    await websocket.accept()
+    try:
+        data = await websocket.receive_json() # 라즈베리 파이 데이터를 가지고 온다.
+
+    except Exception as e:
+        print(e)
+
+@app.websocket("/ws/command/{deviceName}")
+async def websocketCommand(websocket: WebSocket, deviceName: str):
+    await websocket.accept()
+    device = devices.get(deviceName)
+    if device:
+        try:
+            stdout, stderr = await runSshCommand(device['ip_address'], 'ssafy', r'C:\Users\LG\.ssh\id_rsa', 'python3 /home/ssafy/blink.py')
+            if stderr:
+                await websocket.send_text(f"Error executing blink.py: {stderr}")
+            else:
+                await websocket.send_text(f"Blinking LED on {device['description']} successfully started.")
+        except Exception as e:
+            await websocket.send_text(f"Failed to execute blink.py on {device['description']}. Error: {str(e)}")
+    else:
+        await websocket.send_text("Device not found")
+    await websocket.close()
+
+@app.websocket("/ws/status/{deviceName}")
+async def websocketStatus(websocket: WebSocket, deviceName: str):
+    await websocket.accept()
+    try:
+        while True:
+            status = await websocket.receive_text()
+            print(f"Received status from {deviceName}: {status}")
+            
+    except Exception as e:
+        print(f"Error: {str(e)}")
+    finally:
+        await websocket.close()
