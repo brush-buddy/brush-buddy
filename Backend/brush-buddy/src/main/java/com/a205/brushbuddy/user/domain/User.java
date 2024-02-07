2bb280b43828aec554ea5daf29f5c855b999351b
백승윤 <fangdol888@naver.com>
2024-02-07 11:20:25 +0900

    fix : user 타입 integer로 수정

--- Backend/brush-buddy/src/main/java/com/a205/brushbuddy/user/domain/User.java
+++ Backend/brush-buddy/src/main/java/com/a205/brushbuddy/user/domain/User.java
@@ -6,18 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import org.hibernate.annotations.ColumnDefault;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Data;
 
 @Entity
 @Data
@@ -30,7 +19,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id", nullable = false)
-    private Long userId;
+    private Integer userId;
 
     @Column(name = "user_refreshtoken", length = 255)
     private String userRefreshtoken;
