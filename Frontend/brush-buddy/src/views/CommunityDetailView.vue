55caeb2c6e625e82db010fcdbbd8c2714b8236f4
yhw991228 <=>
2024-02-08 14:05:36 +0900

    rebase : rebase for localAxios, searchlist and communitylist

--- Frontend/brush-buddy/src/views/CommunityDetailView.vue
+++ Frontend/brush-buddy/src/views/CommunityDetailView.vue
@@ -1,44 +1,51 @@
 <template>
-    <div>
-        
-        <p class = "communityTitle">{{community["title"]}}</p>
-        
-        <div>
-            
-        </div>
-        <img :src="community" alt="">
-        <p>{{community.contents}}</p>
+  <div>
+    <p class="communityTitle">{{ community["title"] }}</p>
 
-        <p>{{community}}</p>
-    </div>
+    <div></div>
+    <img :src="community" alt="" />
+    <p>{{ community.contents }}</p>
+
+    <p>{{ community }}</p>
+  </div>
 </template>
 
-
 <script setup lang="ts">
-import { useRoute } from 'vue-router';
-import { onMounted, inject, ref } from 'vue';
-import axios from 'axios';
-const route = useRoute()
-const boardId = route.params.id
-const community = ref<any>({ "boardId": -1, 
-        "title": " ", "contents": " ", "thumbnail": " ", "photo": [], "draftId": -1, "likeNumber": 0, "views": 0, "hashtag": [], "createdAt": "-" })
-console.log(route.params)
-onMounted(() => {
-  axios({
-    baseURL: '',
-    method: 'get',
-    url: 'http://localhost:8080/api/v1/board/' + boardId, // URL에 한글이 포함될 경우 인코딩
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8'
-    }
-  }).then(function (response : any) {
-    console.log(response.data)
-    community.value = response.data;
-  })
-})
+import { useRoute } from "vue-router";
+import { onMounted, ref } from "vue";
+import { localAxios } from "../api/axios";
 
+// 보드 id
+const route = useRoute();
+const boardId = route.params.id;
+
+// 초기값
+const community = ref<any>({
+  boardId: -1,
+  title: " ",
+  contents: " ",
+  thumbnail: " ",
+  photo: [],
+  draftId: -1,
+  likeNumber: 0,
+  views: 0,
+  hashtag: [],
+  createdAt: "-",
+});
+
+onMounted(() => {
+  localAxios()({
+    baseURL: "",
+    method: "get",
+    url: "http://localhost:8080/api/v1/board/" + boardId, // URL에 한글이 포함될 경우 인코딩
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+  }).then(function (response: any) {
+    console.log(response.data);
+    community.value = response.data;
+  });
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
