aa4dc98f88bfe06282081242c0c46f4eed13609e
fangdol888 <fangdol888@gmail.com>
2024-02-09 17:35:31 +0900

    fix : Machine 관련 데이터 수정

---
+++ Backend/brush-buddy/src/main/java/com/a205/brushbuddy/machine/dto/MachineRegisterResponseDto.java
@@ -0,0 +1,12 @@
+package com.a205.brushbuddy.machine.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+public class MachineRegisterResponseDto {
+
+}
