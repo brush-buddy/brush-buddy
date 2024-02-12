ca027a3da24da529e080fa5bdfba419fa26013a4
전소민 <goflvhxj96@naver.com>
2024-02-12 15:18:36 +0900

    feat: mypage 팔레트리스트 추가

---
+++ Frontend/brush-buddy/src/components/Diary/CPaletteCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <RouterLink :to="{ name: 'draftDetail', params: { id: palette.paletteId } }">
+    <div id="cardWrapper">
+    {{ palette }}
+        <div id="thumbnailBox">
+            <div id="thumbnail">
+                <!-- <img src="{{ draft.draftThumbnail }}" alt="thumbnail" /> -->
+            </div>
+        </div>
+        <div id="timestamp">
+            <!-- {{ palette.draftTimestamp.substring(0, 10)}} {{ palette.draftTimestamp.substring(11, 19) }} -->
+        </div>
+    </div>
+  </RouterLink>
+</template>
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import { RouterLink } from 'vue-router';
+const prop = defineProps(["palette"]);
+console.log("prop ",prop.palette.paletteId);
+</script>
+<style scoped>
+#cardWrapper {
+  border-style: solid;
+  border-width: 0 0 1px;
+  border-color: gray;
+  padding: 0.5rem 0 1rem;
+}
+#thumbnailBox {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+#thumbnail {
+  background-color: #ffffff;
+  width: 90vw;
+  padding: 1rem;
+  overflow: hidden;
+  > img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+#timestamp {
+  font-size: small;
+  color: gray;
+  margin-left: 1vh;
+}
+</style>
--- Frontend/brush-buddy/src/views/DraftDetailView.vue
+++ Frontend/brush-buddy/src/views/DraftDetailView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div></div>
+  <div>드래프트디테일뷰입니다</div>
 </template>
 
 <style scoped></style>
