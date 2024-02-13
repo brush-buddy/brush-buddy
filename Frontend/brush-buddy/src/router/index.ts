c3f719ed45717fe9774576d3cd75f2535274c502
백승윤 <fangdol888@gmail.com>
2024-02-14 00:21:07 +0900

    feature : QR code 인식 컴포넌트 추가

--- Frontend/brush-buddy/src/router/index.ts
+++ Frontend/brush-buddy/src/router/index.ts
@@ -20,7 +20,6 @@ import VPaletteList from '../components/Diary/VPaletteList.vue'
 import PaletteDetailView from '../views/PaletteDetailView.vue'
 import VMyBoard from '../components/Diary/VMyBoard.vue'
 import HomeListView from '../views/HomeListView.vue'
-import MachineLoginView from '../views/MachineLoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(""),
@@ -146,11 +145,6 @@ const router = createRouter({
       name: 'draftWriteView',
       path: "/draft/write",
       component: DraftPipoMakeViewVue,
-    },
-    {
-      name: "machineLoginView",
-      path: "/machineLogin/:id",
-      component: MachineLoginView,
     }
 
   ]
--- Frontend/brush-buddy/src/stores/user.ts
+++ Frontend/brush-buddy/src/stores/user.ts
@@ -1,15 +1,22 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { ref, type Ref } from "vue";
 
 const useUserStore = defineStore("user", () => {
     const accessToken = ref('');
+    const connectedMachine : Ref<Number> = ref(-1)
     function setAccessToken(newToken:string){
         accessToken.value = newToken
     }
 
+    function setConnectedMachine(machineId : Number){
+        connectedMachine.value = machineId
+    }
+
     return {
         accessToken,
-        setAccessToken
+        setAccessToken,
+        connectedMachine,
+        setConnectedMachine
     }
 })
 
