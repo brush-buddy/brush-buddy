b8282fdb08db4ffda83ea9fc95047074f3faf90e
용희원 <yhw991228@ajou.ac.kr>
2024-02-14 00:20:00 +0900

    feat : add draft purchase

--- Frontend/brush-buddy/src/views/DraftDetailView.vue
+++ Frontend/brush-buddy/src/views/DraftDetailView.vue
@@ -13,7 +13,6 @@ const draft = ref<any>({
   draftPrice: 100,
   draftColorCode: '{"color1": "#FF5733", "color2": "#C70039"}',
   draftThumbnail: 'thumbnail_1.jpg',
-  draftFileLink: 'link_to_file_1',
   draftIsAI: true,
   draftIsPublic: true,
   draftIsDefault: false,
@@ -22,13 +21,15 @@ const draft = ref<any>({
   draftBookmark: 10,
   draftPrompt: 'Sample prompt 1',
   draftTimestamp: '2024-01-29T03:00:00.000+00:00',
+  isAuthor: false,
+  isBookmark: false,
+  isBuy: false,
   categoryContents: []
 })
 
 const im = ref<any>({ order: '-1', imageUrl: '' })
-console.log('hi')
-console.log(im.value)
-
+const charge = ref(false)
+const dialog = ref(false)
 console.log(route.params)
 onMounted(() => {
   localAxios()
@@ -38,6 +39,18 @@ onMounted(() => {
       console.log(draft.value)
     })
 })
+
+const purchase = () => {
+  localAxios()
+    .post('draft/' + draftId + '/purchase')
+    .then((res) => {
+      draft.value.isBuy = true
+    })
+    .catch((err) => {
+      console.log(err)
+      charge.value = true
+    })
+}
 </script>
 
 <template>
@@ -55,6 +68,70 @@ onMounted(() => {
       :image-thumbnail="draft.draftThumbnail"
       :draft-color-code="draft.draftColorCode"
     />
+    <div style="display: flex; justify-content: space-between; width: 100vw; padding: 0 1rem">
+      <div style="height: 4rem; display: flex; align-items: center">
+        <div v-if="draft.isAuthor | draft.isBuy">
+          <v-btn prepend-icon="mdi-download" size="small">다운로드하기</v-btn>
+        </div>
+      </div>
+      <div style="height: 4rem; display: flex; align-items: center">
+        <div v-if="draft.isAuthor">
+          <v-btn prepend-icon="mdi-pencil" size="small">수정하기</v-btn>
+        </div>
+        <div v-if="!draft.isAuthor">
+          <v-btn
+            size="small"
+            prepend-icon="mdi-bucket"
+            v-bind:disabled="draft.isBuy"
+            v-bind:text="!draft.isBuy ? `${draft.draftPrice}$ 구매하기` : '구매한도안'"
+            @click="dialog = true"
+          >
+          </v-btn>
+          <v-dialog v-model="dialog" width="auto">
+            <v-card style="width: 15rem">
+              <div
+                style="
+                  display: flex;
+                  flex-direction: column;
+                  align-items: center;
+                  padding: 1rem 2rem 0rem 2rem;
+                "
+              >
+                <v-card-text> 구매하시겠습니까? </v-card-text>
+                <v-card-text style="font-size: small"
+                  >{{ draft.draftPrice }}마일리지 차감</v-card-text
+                >
+              </div>
+              <v-card-actions style="display: flex; justify-content: flex-end">
+                <v-btn color="primary" @click="(dialog = false), purchase()">구매하기</v-btn>
+                <v-btn color="primary" @click="dialog = false">취소</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-dialog v-model="charge" width="auto">
+            <v-card style="width: 15rem">
+              <div
+                style="
+                  display: flex;
+                  flex-direction: column;
+                  align-items: center;
+                  padding: 1rem 2rem 0rem 2rem;
+                "
+              >
+                <v-card-text> 마일리지가 부족합니다! </v-card-text>
+                <v-card-text style="font-size: small"
+                  >마일리지 충전페이지로 이동하시겠습니까?</v-card-text
+                >
+              </div>
+              <v-card-actions style="display: flex; justify-content: flex-end">
+                <v-btn color="primary" @click="charge = false">이동하기</v-btn>
+                <v-btn color="primary" @click="charge = false">취소</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </div>
+      </div>
+    </div>
     <DraftDetailCommunityThumbnailListComponent :draft-id="Number(draftId)" />
   </div>
 </template>
