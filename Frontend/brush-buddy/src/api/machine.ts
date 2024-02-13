573d968a7ec834d1e98d04b265c1c324f7caf4b0
백승윤 <fangdol888@gmail.com>
2024-02-14 00:11:13 +0900

    feature : front machine axios 작성

--- Frontend/brush-buddy/src/api/machine.ts
+++ Frontend/brush-buddy/src/api/machine.ts
@@ -9,13 +9,28 @@ type MachineRequestType = {
 export type {
     MachineRequestType
 }
-
+// 출력 요청 
 const printPaint = async (
     data : MachineRequestType
 ) => {
     return await localAxios().post("/machine/print", data)
 }
 
+//기기 연결 요청
+const connectMachine = async (
+    machineId : Number
+) => {
+    return await localAxios().post(`/machine/${machineId}`)
+}
+
+//기기 해제 요청
+const disconnectMachine = async (
+) => {
+    return await localAxios().delete(`/machine/disconnect`)
+}
+
 export {
-    printPaint
+    printPaint,
+    connectMachine,
+    disconnectMachine
 }
