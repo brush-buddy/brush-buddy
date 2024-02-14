6814c8e1b362f6f8f07dbd81084142ddc23f6341
이신애 <gosin77@snu.ac.kr>
2024-02-14 15:56:51 +0900

    fix:생성형 ai로 만든 이미지 도안화 하면 파랗게 변하는 현상 수정

--- Ai/brush-buddy/models/awsS3.py
+++ Ai/brush-buddy/models/awsS3.py
@@ -1,3 +1,4 @@
+import io
 import os
 
 import boto3
@@ -26,5 +27,32 @@ class AwsS3(BaseModel):
         except Exception as e:
             print(e)
         else:
-            print("s3 bucket connected!")
+            # print("s3 bucket connected!")
             return s3
+
+    def upload_Pillow_image_to_s3(
+        self, bucket_name, file_key, pillow_image, content_type
+    ):
+        """
+        NumPy 배열 형식의 이미지를 S3 버킷에 업로드합니다.
+        :param bucket_name: S3 버킷 이름
+        :param file_key: 업로드할 파일의 키 (파일 경로와 파일 이름을 포함)
+        :param numpy_array: NumPy 배열 형식의 이미지 데이터
+        :param content_type: 업로드할 파일의 컨텐츠 타입 (예: 'image/jpeg', 'image/png' 등)
+        """
+
+        # Pillow 이미지를 메모리에서 바이너리 데이터로 저장
+        with io.BytesIO() as buffer:
+            pillow_image.save(buffer, format="png")
+            image_binary = buffer.getvalue()
+
+        # S3 클라이언트 생성 및 파일 업로드
+        s3_client = boto3.client("s3")
+        s3_client.put_object(
+            Bucket=bucket_name,
+            Key=file_key,
+            Body=image_binary,
+            ContentType=content_type,
+        )
+
+        return f"https://{bucket_name}.s3.ap-northeast-2.amazonaws.com/{file_key}"
