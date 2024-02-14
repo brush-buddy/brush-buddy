ae92f168cf8cafaa7bd3c086aee411932ae88bb1
용희원 <yhw991228@ajou.ac.kr>
2024-02-15 03:43:00 +0900

    feat : add pay api

--- Backend/brush-buddy/src/main/java/com/a205/brushbuddy/pay/controller/KakaopayController.java
+++ Backend/brush-buddy/src/main/java/com/a205/brushbuddy/pay/controller/KakaopayController.java
@@ -1,11 +1,12 @@
 package com.a205.brushbuddy.pay.controller;
 
+import com.a205.brushbuddy.mileage.domain.MileageLog;
+import com.a205.brushbuddy.mileage.repository.MileageLogRepository;
+import com.a205.brushbuddy.mileage.service.MileageService;
+import com.a205.brushbuddy.pay.client.PayClient;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.a205.brushbuddy.exception.BaseException;
 import com.a205.brushbuddy.exception.ErrorCode;
@@ -20,16 +21,27 @@ import com.a205.brushbuddy.util.JwtUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.Objects;
 
 // TODO : KakaopayReadyRequestDto의 URL(approvalUrl, cancelUrl, failUrl)과 redirectUrl 등 완성 프론트 URL 및 카카오API 연동 필요
 
 @Slf4j
-@RestController("/api/v1/pay")
+@RestController
+@RequestMapping("/api/v1/pay")
 @RequiredArgsConstructor
 public class KakaopayController {
     private final JwtUtil jwtUtil;
     private final KakaopayService kakaopayService;
     private final MileageRepository mileageRepository;
+    private final MileageService mileageService;
+
     //// 브러쉬버디 충전 내역용 workplaceid = 0, 갱신 필요시 참고
     @Value("${spring.kakao.pay.workplaceid}")
     private int workplaceId;
@@ -39,39 +51,85 @@ public class KakaopayController {
     private String partnerUserId;
     private String redirectUrl;
     private String tid;
+    private RestTemplate restTemplate;
+
 
     // 결제 요청 시작
-    @PostMapping
+    @PostMapping("")
     @ResponseBody
     public KakaopayReadyResponseDto ready(KakaopayReadyRequestDto kakaopayReadyRequestDto) {
-        kakaopayService.setKakaopayReadyRequestDto(kakaopayReadyRequestDto);
-        mileageAmount = kakaopayReadyRequestDto.getTotalAmount();
-        partnerOrderId = kakaopayReadyRequestDto.getPartnerOrderId();
-        partnerUserId = kakaopayReadyRequestDto.getPartnerUserId();
-        redirectUrl = kakaopayReadyRequestDto.getRedirectUrl();
-        KakaopayReadyResponseDto kakaopayReadyResponseDto = kakaopayService.sendReadyRequest(kakaopayReadyRequestDto);
-        tid = kakaopayReadyResponseDto.getTid();
-        kakaopayService.setKakaopayReadyResponseDto(kakaopayReadyResponseDto);
-
-        return kakaopayReadyResponseDto;
+//        kakaopayService.setKakaopayReadyRequestDto(kakaopayReadyRequestDto);
+//        mileageAmount = kakaopayReadyRequestDto.getTotalAmount();
+//        partnerOrderId = kakaopayReadyRequestDto.getPartnerOrderId();
+//        partnerUserId = kakaopayReadyRequestDto.getPartnerUserId();
+//        redirectUrl = kakaopayReadyRequestDto.getRedirectUrl();
+//        KakaopayReadyResponseDto kakaopayReadyResponseDto = kakaopayService.sendReadyRequest(kakaopayReadyRequestDto);
+//        tid = kakaopayReadyResponseDto.getTid();
+//        kakaopayService.setKakaopayReadyResponseDto(kakaopayReadyResponseDto);
+//
+//        return kakaopayReadyResponseDto;
+        return null;
     }
 
     // 결제 완료 이후
-    @GetMapping("${spring.kakao.pay.ready.request.approval}")
-    public String completed(@RequestParam("pg_token") String pgToken, HttpServletRequest request) {
-        KakaopayApproveRequestDto kakaopayApproveRequestDto = new KakaopayApproveRequestDto();
-        kakaopayApproveRequestDto.setTid(tid);
-        kakaopayApproveRequestDto.setPartnerOrderId(partnerOrderId);
-        kakaopayApproveRequestDto.setPartnerOrderId(partnerUserId);
-        kakaopayApproveRequestDto.setPgToken(pgToken);
-        kakaopayService.setKakaopayApproveRequestDto(kakaopayApproveRequestDto);
+//    @GetMapping("${spring.kakao.pay.ready.request.approval}")
+//    public String completed(@RequestParam("pg_token") String pgToken, HttpServletRequest request) {
+//        KakaopayApproveRequestDto kakaopayApproveRequestDto = new KakaopayApproveRequestDto();
+//        kakaopayApproveRequestDto.setTid(tid);
+//        kakaopayApproveRequestDto.setPartnerOrderId(partnerOrderId);
+//        kakaopayApproveRequestDto.setPartnerOrderId(partnerUserId);
+//        kakaopayApproveRequestDto.setPgToken(pgToken);
+//        kakaopayService.setKakaopayApproveRequestDto(kakaopayApproveRequestDto);
+//
+//        KakaopayApproveResponseDto kakaopayApproveResponseDto = kakaopayService
+//            .sendApproveRequest(kakaopayApproveRequestDto);
+//        kakaopayService.setKakaopayApproveResponseDto(kakaopayApproveResponseDto);
+//
+//        Integer userId = jwtUtil.getUserId(request).orElseThrow(() -> new BaseException(ErrorCode.UNAUTHORIZED));
+//
+//        return "redirect:" + redirectUrl;
+//    }
 
-        KakaopayApproveResponseDto kakaopayApproveResponseDto = kakaopayService
-            .sendApproveRequest(kakaopayApproveRequestDto);
-        kakaopayService.setKakaopayApproveResponseDto(kakaopayApproveResponseDto);
 
-        Integer userId = jwtUtil.getUserId(request).orElseThrow(() -> new BaseException(ErrorCode.UNAUTHORIZED));
+    @PostMapping("/makePayment")
+    public ResponseEntity<?> makePayment(HttpServletRequest request, @RequestParam("mileage") int mileage) {
 
-        return "redirect:" + redirectUrl;
+        Integer userId = jwtUtil.getUserId(request)
+                .orElseThrow(() -> new BaseException(
+                        ErrorCode.INVALID_TOKEN));// 헤더의 access token으로 userId 추출, null 반환시 유효하지 않은 토큰 오류 전송
+        Long mileageNumber = mileageService.requestOrderInfo(mileage, userId);
+        if (mileageNumber != null) {
+            ResponseEntity<KakaopayReadyResponseDto> payApprove = kakaopayService.sendApprove(
+                    KakaopayApproveRequestDto.builder().cid("TC0ONETIME").partner_order_id(mileageNumber.toString())
+                            .partner_user_id(String.valueOf(userId)).item_name("mileage").quantity("1")
+                            .total_amount(String.valueOf(mileage)).tax_free_amount("0")
+                            .approval_url("https://brush-buddy.duckdns.org/pay/complete")
+                            .cancel_url("https://brush-buddy.duckdns.org/pay/cancel")
+                            .fail_url("https://brush-buddy.duckdns.org/pay/fail").build());
+            String tid = Objects.requireNonNull(payApprove.getBody()).getTid();
+            mileageService.setMileageTid(mileageNumber, tid);
+            return ResponseEntity.ok(payApprove);
+
+        }
+        throw new BaseException(ErrorCode.NOT_PRIVIEGED);
     }
+
+    @PostMapping("/complete")
+    public ResponseEntity<?> makeSave(HttpServletRequest request, @RequestParam("pg_token") String pgToken,@RequestParam("tid")
+                                      String tid) {
+        Integer userId = jwtUtil.getUserId(request)
+                .orElseThrow(() -> new BaseException(
+                        ErrorCode.INVALID_TOKEN));// 헤더의 access token으로 userId 추출, null 반환시 유효하지 않은 토큰 오류 전송
+
+        MileageLog mileageLog = mileageService.getMileageLog(tid);
+
+        KakaopayReadyRequestDto kakaopayApproveRequestDto = KakaopayReadyRequestDto.builder().cid("TC0ONETIME").tid(tid)
+                .partner_order_id(mileageLog.getMileageLogId().toString()).partner_user_id(String.valueOf(userId))
+                .pg_token(pgToken).build();
+
+
+
+        return kakaopayService.sendComplete(kakaopayApproveRequestDto);
+    }
+
 }
--- Frontend/brush-buddy/src/views/payCharge.vue
+++ Frontend/brush-buddy/src/views/payCharge.vue
@@ -5,10 +5,10 @@ import { usePayStore } from '@/stores/pay'
 import { ref } from 'vue'
 
 const user = navigator.userAgent
-const price = ref<Number>(1000)
-const purchase = () => {
+// const price = ref<Number>(1000)
+const purchase = (price: Number) => {
   localAxios()
-    .post(`pay/makePayment?mileage=${price.value}`)
+    .post(`pay/makePayment?mileage=${price}`)
     .then((res) => {
       console.log('결제 요청 성공')
       usePayStore().setTid(res.data.body.tid)
@@ -24,7 +24,26 @@ const purchase = () => {
 </script>
 
 <template>
-  <div><v-btn @click="purchase()"></v-btn></div>
+  <div style="display: flex; flex-direction: column; align-items: center; margin-top: 2rem">
+    <h1 style="margin-bottom: 1rem">마일리지 충전</h1>
+    <template v-for="item in [100, 300, 500, 1000, 3000, 5000]" :key="item">
+      <div class="buttonRow">
+        <p>{{ item }} 마일리지</p>
+        <v-btn @click="purchase(item)">{{ item }}</v-btn>
+      </div>
+    </template>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.buttonRow {
+  display: flex;
+  width: 90vw;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 2rem;
+  padding: 2rem 1rem;
+  border-top: 1px solid #e0e0e0;
+  border-bottom: 1px solid #e0e0e0;
+}
+</style>
