395a05f54457e66c6a781e437d6165756e1bdefa
용희원 <yhw991228@ajou.ac.kr>
2024-02-15 03:43:00 +0900

    feat : add pay api

--- Backend/brush-buddy/src/main/java/com/a205/brushbuddy/mileage/controller/MileageController.java
+++ Backend/brush-buddy/src/main/java/com/a205/brushbuddy/mileage/controller/MileageController.java
@@ -5,11 +5,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.a205.brushbuddy.exception.BaseException;
 import com.a205.brushbuddy.exception.ErrorCode;
@@ -23,6 +19,10 @@ import com.a205.brushbuddy.util.JwtUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Slf4j
 @RestController
@@ -57,5 +57,58 @@ public class MileageController {
             .build();
 
         return new ResponseEntity<MileageHistoryResponseDto>(mileageHistoryResponseDto, HttpStatus.OK);
+    }
+
+    @PostMapping("/make-payment")
+    public String makePayment(HttpServletRequest request, @RequestParam("mileage") int mileage) {
+
+        Integer userId = jwtUtil.getUserId(request)
+                .orElseThrow(() -> new BaseException(
+                        ErrorCode.INVALID_TOKEN));// 헤더의 access token으로 userId 추출, null 반환시 유효하지 않은 토큰 오류 전송
+        try {
+            URL url = new URL("https://open-api.kakaopay.com/online/v1/payment/ready");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Authorization", "SECRET_KEY csM1qC31YmUpHg2Rt74s3EOOBKqdzHWI");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setDoOutput(true);
+            // 구매 정보를 들고 옵니다.
+            Long mileageNumber = mileageService.requestOrderInfo(mileage, userId);
+
+
+            // 구매정보의 pk를 order id로 두고 보내줍니다.
+            String params =
+                    "cid=TC0ONETIME&" +
+                            "partner_order_id=" + mileageNumber +
+                            "&partner_user_id=" + userId +
+                            "&item_name=마일리지_충전" +
+                            "&quantity=1" +
+                            "&total_amount=" + mileage +
+                            "&tax_free_amount=0" +
+                            "&approval_url="+ "http://localhost:5173/pay/success" +
+                            "&cancel_url=" + "http://localhost:5173/pay/cancel" +
+                            "&fail_url=" + "http://localhost:5173/pay/fail";
+            OutputStream os = conn.getOutputStream();
+            DataOutputStream writer = new DataOutputStream(os);
+            writer.writeBytes(params);
+            writer.flush();
+            writer.close();
+
+            int responseCode = conn.getResponseCode();
+
+            InputStream is;
+            if(responseCode== 200) {
+                is = conn.getInputStream();
+            } else {
+                is = conn.getErrorStream();
+            }
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            return br.readLine();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
--- Backend/brush-buddy/src/main/java/com/a205/brushbuddy/pay/service/KakaopayService.java
+++ Backend/brush-buddy/src/main/java/com/a205/brushbuddy/pay/service/KakaopayService.java
@@ -1,8 +1,12 @@
 package com.a205.brushbuddy.pay.service;
 
+import com.a205.brushbuddy.pay.client.PayClient;
+import com.amazonaws.Response;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -20,7 +24,7 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@NoArgsConstructor
+@RequiredArgsConstructor
 @Service
 public class KakaopayService {
     private static final String[] headerKey = {"Authorization", "Content-type"};
@@ -45,6 +49,7 @@ public class KakaopayService {
     @Setter
     private KakaopayApproveResponseDto kakaopayApproveResponseDto;
 
+    private final PayClient payClient;
     @PostConstruct
     public void setHeader() {
         MultiValueMap<String, String> headers = new LinkedMultiValueMap<String, String>();
@@ -54,44 +59,53 @@ public class KakaopayService {
     }
 
     public KakaopayReadyResponseDto sendReadyRequest(KakaopayReadyRequestDto kakaopayReadyRequestDto) {
-        MultiValueMap<String, String> body = new LinkedMultiValueMap<String, String>();
-
-        log.info("sendReadyRequest HTTP Entitiy body :");
-        String[] value = kakaopayReadyRequestDto.getValue();
-        for (int i = 0; i < KakaopayReadyRequestDto.size; ++i) {
-            body.add(KakaopayReadyRequestDto.name[i], value[i]);
-            log.info(KakaopayReadyRequestDto.name[i] + ":" + value[i]);
-        }
-
-        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<MultiValueMap<String, String>>(body,
-            headers);
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        kakaopayReadyResponseDto = restTemplate.postForObject(readyRequestUrl, httpEntity,
-            KakaopayReadyResponseDto.class);
-
-        return kakaopayReadyResponseDto;
+//        MultiValueMap<String, String> body = new LinkedMultiValueMap<String, String>();
+//
+//        log.info("sendReadyRequest HTTP Entitiy body :");
+//        String[] value = kakaopayReadyRequestDto.getValue();
+//        for (int i = 0; i < KakaopayReadyRequestDto.size; ++i) {
+//            body.add(KakaopayReadyRequestDto.name[i], value[i]);
+//            log.info(KakaopayReadyRequestDto.name[i] + ":" + value[i]);
+//        }
+//
+//        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<MultiValueMap<String, String>>(body,
+//            headers);
+//
+//        RestTemplate restTemplate = new RestTemplate();
+//
+//        kakaopayReadyResponseDto = restTemplate.postForObject(readyRequestUrl, httpEntity,
+//            KakaopayReadyResponseDto.class);
+//
+//        return kakaopayReadyResponseDto;
+        return null;
     }
 
     public KakaopayApproveResponseDto sendApproveRequest(KakaopayApproveRequestDto kakaopayApproveRequestDto) {
-        MultiValueMap<String, String> body = new LinkedMultiValueMap<String, String>();
-
-        String[] value = kakaopayApproveRequestDto.getValue();
-        for (int i = 0; i < KakaopayApproveRequestDto.size; ++i) {
-            body.add(KakaopayApproveRequestDto.name[i], value[i]);
-            log.info(KakaopayApproveRequestDto.name[i] + ":" + value[i]);
-        }
-
-        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<MultiValueMap<String, String>>(body,
-            headers);
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        kakaopayApproveResponseDto = restTemplate.postForObject(approveRequestUrl, httpEntity,
-            KakaopayApproveResponseDto.class);
-
-        return kakaopayApproveResponseDto;
+//        MultiValueMap<String, String> body = new LinkedMultiValueMap<String, String>();
+//
+//        String[] value = kakaopayApproveRequestDto.getValue();
+//        for (int i = 0; i < KakaopayApproveRequestDto.size; ++i) {
+//            body.add(KakaopayApproveRequestDto.name[i], value[i]);
+//            log.info(KakaopayApproveRequestDto.name[i] + ":" + value[i]);
+//        }
+//
+//        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<MultiValueMap<String, String>>(body,
+//            headers);
+//
+//        RestTemplate restTemplate = new RestTemplate();
+//
+//        kakaopayApproveResponseDto = restTemplate.postForObject(approveRequestUrl, httpEntity,
+//            KakaopayApproveResponseDto.class);
+//
+//        return kakaopayApproveResponseDto;
+        return null;
     }
 
+    public ResponseEntity<KakaopayReadyResponseDto> sendApprove(KakaopayApproveRequestDto kakaopayApproveRequestDto) {
+        return payClient.makePayment(kakaopayApproveRequestDto);
+    }
+
+    public ResponseEntity<?> sendComplete(KakaopayReadyRequestDto kakaopayReadyRequestDto) {
+        return payClient.savePayment(kakaopayReadyRequestDto);
+    }
 }
