971c0ad9e6bdfa23a4ffe010c49d64adc7019ee4
김영진 <young285j@gmail.com>
2024-02-14 09:37:34 +0900

    feat : component of Mileage

---
+++ Frontend/brush-buddy/src/components/Mileage/MileageChargeComponent.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>충전</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
---
+++ Frontend/brush-buddy/src/components/Mileage/MileageHistoryComponent.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>내역</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
